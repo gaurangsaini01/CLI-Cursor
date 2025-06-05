127e86d9f688c317781c76044f4bb0852d287c0f
Gaurang Saini <gaurangsaini01@gmail.com>
2025-06-05 16:22:33 +0530

    Final Commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import json
 
 client = OpenAI()
+print("WELCOME TO MINI COMMAND LINE BASED CURSOR !")
 
 def run_command(cmd:str):
     res = os.system(cmd)
@@ -15,13 +16,14 @@ available_tools = {
 }
 
 SYSTEM_PROMPT = """
-    you are an AI agent that builds projects for user straight from the CLI . 
+
+    you are an AI agent that builds projects for user straight from the CLI or if user asks simple question and doesn;t ask you to do anything , just simply give him the answer no need to follow the steps mentioned below.
     You need to follow the following steps : start , plan, action ,observe. 
 
     For the given user query and available tools, plan the step by step execution, based on the planning,
     select the relevant tool from the available tool. and based on the tool selection you perform an action to call the tool.
 
-    # Make sure you never give commands for removing anything from system files . 
+    NEVER use `rm`, `sudo`, or delete system files.
 
     Available Tools:
     1. "run_command":Takes linux command as a string and executes the command and returns the output after executing it.
@@ -30,6 +32,7 @@ SYSTEM_PROMPT = """
     - Follow the Output JSON Format.
     - Always perform one step at a time and wait for next input
     - Carefully analyse the user query
+    - NEVER use `rm`, `sudo`, or delete system files.
 
     Output Format : 
     {{
@@ -46,15 +49,25 @@ SYSTEM_PROMPT = """
     Output : {{"step":"action","function":"run_command","input":"mkdir raju"}}
     Output : {{"step":"observe","content":"the folder has been created"}}
     Output : {{"step":"result","content":"Folder created Successfully ."}}
+
+    Example:
+    User: Create a todo app in react .
     
+    you should run "npm create vite@latest {{name if mentioned by user otherwise my-app}} -- --template react"
+    or
+    if user mentions typescript : "npm create vite@latest {{name if mentioned by user otherwise my-app}} -- --template react-ts"
+
+    then start writing code for making a todo application file by file as needed , follow proper Component structure and make sure to place them inside App file.
+    
+    make sure to run npm install, then run the server using command : "cd {{name of project}} && npm run dev"
 """
 messages = [{"role":"system","content":SYSTEM_PROMPT}]
 while True:
-    query = input("Enter: ")
-    messages.append({"role":"user","content":query})
+    query = input("Enter the query: ")
+    messages.append({"role" : "user","content" : query})
     while True:
         response = client.chat.completions.create(
-            model = "gpt-4o",
+            model = "gpt-4.1",
             response_format = {"type":"json_object"},
             messages = messages,
         )
@@ -72,6 +85,6 @@ while True:
             messages.append({ "role": "assistant", "content": json.dumps({ "step": "observe", "content": result }) })
             continue
         if step == "result":
-            print("🤖: ",content)
+            print("🤖: \n",content)
             break
 
